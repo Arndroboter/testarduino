ef9f708cf59d60470e1880c23ac456f156c0b35b
Arnd <Arndroboter@users.noreply.github.com>
2016-12-13 18:15:01 +0100

    Add files via upload
    Lichtscharanke uploaded

---
+++ LichtschrankeBsp.ino
@@ -0,0 +1,37 @@
+/*
+  AnalogReadSerial abgewandelt
+  Für die Steuerung einer Lichtschranke mit FT:
+  - mit weiss an +5V
+  - mit rot an 1. Analog in: A0 und 2. über Potenzometer oder Widerstand an _|_ GND zum Entladen des Anschlusses
+
+  Gibt aus: 1 und 0
+
+  verändern von: Delay (wird konstanter
+*/
+
+void setup() {
+  Serial.begin(9600);
+  //Bitte aendern: - wait (normal 3) macht das Signal stabiler
+  //               - switching (normal 40) schwellwert zum Schalten von 0 auf 1
+
+}
+
+void loop() {
+  int wait = 5;
+  int switching = 40;
+  int sensorValue = analogRead(A0);
+  delay(wait);
+  sensorValue = sensorValue + analogRead(A0);
+  delay(wait);
+  sensorValue = sensorValue + analogRead(A0);
+  delay(wait);
+  sensorValue = sensorValue + analogRead(A0);
+  int printer = sensorValue;
+ if(printer > switching){
+    printer = 1;
+  }
+  else{
+    printer = 0;
+  }
+  Serial.println(printer);
+}
